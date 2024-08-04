52edb5af96ff8b99d1e9e34ab2a243bcf73d560e
Falerey <104234607+Falerey@users.noreply.github.com>
2024-08-04 03:59:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,22 @@ document.getElementById('contact-form').addEventListener('submit', function(even
     event.preventDefault();
     alert('Form submitted!');
 });
+
+<script>
+    // JavaScript para mostrar/ocultar la flecha y desplazarse hacia arriba
+    window.addEventListener('scroll', function() {
+        var backToTopButton = document.getElementById('backToTop');
+        if (window.scrollY > 300) {
+            backToTopButton.style.display = 'block';
+        } else {
+            backToTopButton.style.display = 'none';
+        }
+    });
+
+    document.getElementById('backToTop').addEventListener('click', function() {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+</script>
